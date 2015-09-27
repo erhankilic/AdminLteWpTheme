3e3a1bd0137f5c92f18cb730eb46329901b646cc
Erhan Kılıc <info@ideayazilim.net>
2015-09-27 17:20:52 +0300

    page.php has been completed.

---
+++ page.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                <?php the_title(); ?>
+            </h1>
+            <ol class="breadcrumb">
+                <?php breadcrums() ?>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content" id="post">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="box box-primary">
+                        <div class="box-body">
+
+                            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                                <div class="post">
+                                    <div class='description'>
+                                        <h2><?php the_title(); ?></h2>
+                                    </div>
+                                    <div class="post-content">
+                                        <?php the_content();?>
+                                    </div>
+                                </div>
+                                <!-- /.post -->
+                            <?php endwhile; ?>
+                            <?php endif; ?>
+
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+
+        </section>
+        <!-- /.content -->
+    </div><!-- /.content-wrapper -->
+
+<?php get_footer(); ?>
