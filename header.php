ee7744e676191662b59df6133aa53f1849fd7e92
Erhan Kılıc <erhan_kilic@outlook.com>
2016-06-02 09:46:15 +0300

    Header updated.

--- header.php
+++ header.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset=<?php bloginfo('charset'); ?>" />
-    <meta http-equiv=" X-UA-Compatible
-    " content="IE=edge">
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?php the_title(); ?></title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
